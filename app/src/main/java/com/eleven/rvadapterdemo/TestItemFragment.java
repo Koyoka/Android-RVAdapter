5fb9414b4ac4f7e3bf2621ac24f26637301e2ec0
elevenrz <elevenrz@126.com>
2018-09-12 16:58:33 +0800

    回滚修改build.gradle

--- app/src/main/java/com/eleven/rvadapterdemo/TestItemFragment.java
+++ app/src/main/java/com/eleven/rvadapterdemo/TestItemFragment.java
@@ -1,21 +1,21 @@
-//package com.eleven.rvadapterdemo;
-//
-//import android.widget.Toast;
-//
-//import com.eleven.rvadapter.extend.AutoTestItemFragment;
-//
-//public class TestItemFragment extends AutoTestItemFragment {
-//    @Override
-//    protected TestFuncs buildTestFuncs() {
-//
-//        return new TestFuncs() {
-//            @Override
-//            public void testItem() {
-//
-//                addd("测试Toast", () -> {
-//                    Toast.makeText(getContext(), "111", Toast.LENGTH_SHORT).show();
-//                });
-//            }
-//        };
-//    }
-//}
+package com.eleven.rvadapterdemo;
+
+import android.widget.Toast;
+
+import com.eleven.rvadapter.extend.AutoTestItemFragment;
+
+public class TestItemFragment extends AutoTestItemFragment {
+    @Override
+    protected TestFuncs buildTestFuncs() {
+
+        return new TestFuncs() {
+            @Override
+            public void testItem() {
+
+                addd("测试Toast", () -> {
+                    Toast.makeText(getContext(), "111", Toast.LENGTH_SHORT).show();
+                });
+            }
+        };
+    }
+}
